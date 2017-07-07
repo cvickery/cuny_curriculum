96bca441ae1212ba6f409657399b42a1c59938d2
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2017-07-06 22:32:43 -0400

    Initial version of external_subject.py

---
+++ external_subjects.py
@@ -0,0 +1,22 @@
+# Clear and re-populate the external_subjects table.
+
+import sqlite3
+import csv
+
+db = sqlite3.connect('courses.db')
+cur = db.cursor()
+cur.execute('delete from external_subjects')
+with open('QNS_QCCV_EXTERNAL_SUBJECT_TBL.csv') as csvfile:
+  csv_reader = csv.reader(csvfile)
+  cols = None
+  for row in csv_reader:
+    if cols == None:
+      row[0] = row[0].replace('\ufeff', '')
+      cols = [val.lower().replace(' ', '_').replace('/', '_') for val in row]
+    else:
+      q = """insert into external_subjects values('{}', '{}')""".format(
+          row[cols.index('external_subject_area')],
+          row[cols.index('description')].replace("'", "’"))
+      cur.execute(q)
+  db.commit()
+  db.close()
