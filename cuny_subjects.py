d711e0ffb4081790b618c0c59a59f75d13b8ae62
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2017-11-16 09:48:00 -0500

    Combine disciplines and cuny_subjects initializations in cuny_subjects.py

--- cuny_subjects.py
+++ cuny_subjects.py
@@ -1,4 +1,5 @@
-# Clear and re-populate the (course) attributes table.
+# Clear and re-populate the table of internal subjects at all cuny colleges (disciplines).
+# Clear and re-populate the table of external subject areas (cuny_subjects).
 
 import os
 import re
@@ -8,36 +9,51 @@ import argparse
 from datetime import date
 from collections import namedtuple
 
-parser = argparse.ArgumentParser('Create subjects table from CUNY’s')
+parser = argparse.ArgumentParser('Create internal and external subject tables')
 parser.add_argument('--debug', '-d', action='store_true')
 args = parser.parse_args()
 
-# Be sure there is a subject query file, and get the latest one.
+# Internal Subjects (disciplines)
+# Be sure there are subject_tbl (disciplines) and external_subject (cuny_subjects) query files,
+# and get the latest ones.
 all_files = [x for x in os.listdir('./queries/') if x.endswith('.csv')]
-latest_subj = '0000-00-00'
-subj_file = None
+latest_discp = '0000-00-00'
+discp_file = None
+latest_extern = '0000-00-00'
+extern_file = None
 for file in all_files:
   mdate = date.fromtimestamp(os.lstat('./queries/' + file).st_mtime).strftime('%Y-%m-%d')
-  if re.search('_subject_', file, re.I) and mdate > latest_subj:
+  if re.search('_subject_tbl', file, re.I) and mdate > latest_discp:
+    latest_extern = mdate
+    extern_file = file
+  if re.search('_cuny_subject_', file, re.I) and mdate > latest_extern:
     latest_subj = mdate
-    subj_file = file
+    discp_file = file
 
-if subj_file == None:
-  print('No subject table query found')
-if args.debug: print('cuny_subjects.py using', subj_file)
+if discp_file == None:
+  print('No subject_tbl (disciplines) query found')
+  exit()
+if extern_file == None:
+  print('No cuny_subject (cuny_subjects) query found')
+
+if args.debug: print('cuny_subjects.py:\n  disciplines: {}\n  cuny_subjects:{}'.format(discp_file,
+                                                                                       extern_file))
+
 db = psycopg2.connect('dbname=cuny_courses')
 cur = db.cursor()
-cur.execute('drop table if exists subjects')
+cur.execute('drop table if exists disciplines')
 cur.execute(
     """
-    create table subjects (
+    create table disciplines (
       institution text references institutions,
-      subject text,
+      discipline text,
       description text,
-      primary key (institution, subject))
+      primary key (institution, discipline))
     """)
 
-with open('./queries/{}'.format(subj_file)) as csvfile:
+# College-specific disciplines
+# ----------------------------
+with open('./queries/' + discp_file) as csvfile:
   csv_reader = csv.reader(csvfile)
   cols = None
   for row in csv_reader:
@@ -49,11 +65,37 @@ with open('./queries/{}'.format(subj_file)) as csvfile:
     else:
       record = Record._make(row)
       if record.institution != 'UAPC1':
-        q = """insert into subjects values ('{}', '{}', '{}') on conflict do nothing""".format(
+        q = """insert into disciplines values ('{}', '{}', '{}') on conflict do nothing""".format(
             record.institution,
             record.subject,
             record.formal_description.replace('\'', '’'))
         if args.debug: print(q)
         cur.execute(q)
   db.commit()
-  db.close()
+
+# CUNY ("external") subjects
+# --------------------------
+cur.execute('drop table if exists cuny_subjects cascade')
+cur.execute("""
+  create table cuny_subjects (
+  area text primary key,
+  description text
+  )
+  """)
+with open('./queries/' + extern_file) as csvfile:
+  csv_reader = csv.reader(csvfile)
+  cols = None
+  for row in csv_reader:
+    if cols == None:
+      row[0] = row[0].replace('\ufeff', '')
+      cols = [val.lower().replace(' ', '_').replace('/', '_') for val in row]
+      Record = namedtuple('Record', cols)
+    else:
+      record = Record._make(row)
+      q = """insert into cuny_subjects values('{}', '{}')""".format(
+          record.external_subject_area,
+          record.description.replace("'", "’"))
+      cur.execute(q)
+  db.commit()
+
+db.close()
