05c6065cd7810e56aadd7a76c612c8144c13e7ec
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2017-12-21 18:05:12 -0500

    Cleaning up dump-restore for better automation.
    Not ready to cron it yet, though.

--- dump_restore.sh
+++ dump_restore.sh
@@ -1,51 +1,61 @@
 #! /usr/local/bin/bash
 
-# 2017-11-21
-
-# To copy the cuny_courses db from here to the cloud is complicated by the fact that Google App Engine
-# (GAE) is running an older version of Postgres than I am running on babbage and cvlaptop.
-# (Google: 9.6.1; Me: 10.1)
+# 2017-11-21: To copy the cuny_courses db from here to the cloud is complicated by the fact that
+# Google App Engine (GAE) is running an older version of Postgres than I am running on babbage and
+# cvlaptop. (Google: 9.6.1; Me: 10.1)
 
 (
- . ~/.aliases_du_jour
-# First, be sure sql proxy is not running and create a dump of the local db:
-echo Dumping ...
+. ~/.aliases_du_jour > /dev/null
 restore_file=restore.`date +%Y-%m-%d`.sql
+# First, be sure sql proxy is not running and create a dump of the local db:
+echo "Be sure pgproxy is stopped (look for 'No matching processes')..."
 pgproxy stop
+echo "Dumping local db..."
 pg_dump -O cuny_courses > $restore_file
-
-read -p "Edit $restore_file before continuing ..." reply
 
 # No user associated with tables. On babbage and cvlaptop, vickery is the owner, but on GAE, the
 # owner will be postgres.
 
-# Now there are issues with restore.sql that have to be fixed.
+# There are issues with restore_file that have to be fixed.
 #   - The plpgsql extension is not owned by postgres on GAE, so commands related to that generate
-#   errors. But they don't prevent a successful restore. This may change if triggers written in
-#   plpgsql get written.
-#   - At the time I am writing this, everything was owned by vickery and I edited the sql to change
-#   all to postgres. This should be unnecessary once these instructions are debugged.
+#     errors. But they don't prevent a successful restore. This may change if triggers written in
+#     plpgsql get written.
 #   - In Postgres 10.1, the create sequence command has a new "AS <datatype>" clause that causes an
-#   error when restoring to the 9.6.1 server. I just deleted that line from the SQL, and the
-#   restore went fine.
+#     error when restoring to the 9.6.1 server on GAE. You have to delete those lines from
+#     restore_file.
+echo "Fixing $restore_file ..."
+ack -v "plpgsql" $restore_file | ack -v "AS integer" > temp.sql
+diff $restore_file temp.sql
+read -p "Restore to GAE [Yn]? " reply
+if [[ $reply =~ [Nn] ]]
+then rm temp.sql
+      echo "Remove '$restore_file' if you don’t want it."
+      echo "Nothing else changed."
+      exit
+fi
+mv temp.sql $restore_file
 
-# There is an option to specify a directory for the dump, which might be more efficient. But the
-# files generated are compressed, so it seems like less bother to use the default format, which
+# Aside: There is an option to specify a directory for the dump, which might be more efficient. But
+# the files generated are compressed, so it seems like less bother to use the default format, which
 # produces more easily-edited output. The restore took only 17 seconds, so there's not much to be
 # gained by generating the more efficient dump directory.
 
 # To restore to the GAE:
 #   Start the SQL proxy server, delete and recreate the db, and restore as user postgres:
-echo Restoring ...
+echo "Restore $restore_file to GAE..."
+echo "  Starting pgproxy ..."
 pgproxy start
 sleep 3
-echo '  drop database...'
+echo '  Dropping GAE database...'
 psql -U postgres -c 'drop database cuny_courses'
 sleep 2
-echo '  create database...'
+echo '  Creating GAE database...'
 psql -U postgres -c 'create database cuny_courses'
 sleep 2
-echo '  restore database...'
+echo '  Rebuilding GAE database...'
 psql -U postgres cuny_courses < $restore_file > restore.log 2>&1
-
+if [ $? -eq 0 ]
+then echo "OK"
+else echo "Error: see restore.log for details."
+fi
 )
