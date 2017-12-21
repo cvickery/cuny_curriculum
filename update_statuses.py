8a999f42e962f2043565e4c93604e347ded86ce1
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2017-12-21 09:36:12 -0500

    Small bug fixes to initialize_db.sh and update_statuses.py

--- update_statuses.py
+++ update_statuses.py
@@ -12,7 +12,7 @@ Event_Type = namedtuple('Event_Type', [d[0] for d in cursor.description])
 event_types = map(Event_Type._make, cursor.fetchall())
 bitmasks = dict()
 for event_type in event_types:
-  bitmasks[event_type.abbr] = event_type.value
+  bitmasks[event_type.abbr] = event_type.bitmask
 
 cursor.execute('select * from events')
 Event = namedtuple('Event', [d[0] for d in cursor.description])
@@ -29,9 +29,9 @@ for event in events:
                             event.group_number,
                             event.destination_institution))
   status = cursor.fetchone()[0]
-  print('status is {}\n  event_type is {}\n  bitmask is {}'.format(status, event.event_type, bitmasks[event.event_type]))
+  # print('status is {}\n  event_type is {}\n  bitmask is {}'.format(status, event.event_type, bitmasks[event.event_type]))
   status = status | bitmasks[event.event_type]
-  print('new status:', status)
+  # print('new status:', status)
   cursor.execute("""
                   update rule_groups set status = {}
                    where source_institution = '{}'
