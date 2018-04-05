9c2a8f83f55f4cdfa26a6d99b60cf6d8d3d62e80
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2018-04-04 22:49:07 -0400

    Add associates/baccalureate cols to institutions.

--- institutions.sql
+++ institutions.sql
@@ -3,28 +3,30 @@ create table institutions (
   code text primary key,
   prompt text,
   name text,
+  associates boolean,
+  bachelors boolean,
   date_updated date default now());
 --                                code     prompt            name
-insert into institutions values ('BAR01', 'Baruch',         'Baruch College');
-insert into institutions values ('BCC01', 'Bronx',          'Bronx Community College');
-insert into institutions values ('BKL01', 'Brooklyn',       'Brooklyn College');
-insert into institutions values ('BMC01', 'BMCC',           'Borough of Manhattan CC');
-insert into institutions values ('CSI01', 'Staten Island',  'College of Staten Island');
-insert into institutions values ('CTY01', 'City',           'City College');
-insert into institutions values ('GRD01', 'Grad Center',    'The Graduate Center');
-insert into institutions values ('HOS01', 'Hostos',         'Hostos Community College');
-insert into institutions values ('HTR01', 'Hunter',         'Hunter College');
-insert into institutions values ('JJC01', 'John Jay',       'John Jay College');
-insert into institutions values ('KCC01', 'Kingsborough',   'Kingsborough CC');
-insert into institutions values ('LAG01', 'LaGuardia',      'LaGuardia Community College');
-insert into institutions values ('LAW01', 'Law School',     'CUNY School of Law');
-insert into institutions values ('LEH01', 'Lehman',         'Lehman College');
-insert into institutions values ('MEC01', 'Medgar Evers',   'Medgar Evers College');
-insert into institutions values ('MED01', 'Medical School', 'CUNY School of Medicine');
-insert into institutions values ('NCC01', 'Guttman',        'Guttman Community College');
-insert into institutions values ('NYT01', 'City Tech',      'NYC College of Technology');
-insert into institutions values ('QCC01', 'Queensborough',  'Queensborough CC');
-insert into institutions values ('QNS01', 'Queens',         'Queens College');
-insert into institutions values ('SPH01', 'Public Health',  'CUNY School of Public Health');
-insert into institutions values ('SPS01', 'SPS',            'School of Professional Studies');
-insert into institutions values ('YRK01', 'York',           'York College');
+insert into institutions values ('BAR01', 'Baruch',         'Baruch College',                 false, true);
+insert into institutions values ('BCC01', 'Bronx',          'Bronx Community College',        true,  false);
+insert into institutions values ('BKL01', 'Brooklyn',       'Brooklyn College',               false, true);
+insert into institutions values ('BMC01', 'BMCC',           'Borough of Manhattan CC',        true,  false);
+insert into institutions values ('CSI01', 'Staten Island',  'College of Staten Island',       true,  true);
+insert into institutions values ('CTY01', 'City',           'City College',                   false, true);
+insert into institutions values ('GRD01', 'Grad Center',    'The Graduate Center',            false, false);
+insert into institutions values ('HOS01', 'Hostos',         'Hostos Community College',       true,  false);
+insert into institutions values ('HTR01', 'Hunter',         'Hunter College',                 false, true);
+insert into institutions values ('JJC01', 'John Jay',       'John Jay College',               true,  true);
+insert into institutions values ('KCC01', 'Kingsborough',   'Kingsborough CC',                true,  false);
+insert into institutions values ('LAG01', 'LaGuardia',      'LaGuardia Community College',    true,  false);
+insert into institutions values ('LAW01', 'Law School',     'CUNY School of Law',             false, false);
+insert into institutions values ('LEH01', 'Lehman',         'Lehman College',                 false, true);
+insert into institutions values ('MEC01', 'Medgar Evers',   'Medgar Evers College',           true,  true);
+insert into institutions values ('MED01', 'Medical School', 'CUNY School of Medicine',        false, false);
+insert into institutions values ('NCC01', 'Guttman',        'Guttman Community College',      true,  false);
+insert into institutions values ('NYT01', 'City Tech',      'NYC College of Technology',      true,  true);
+insert into institutions values ('QCC01', 'Queensborough',  'Queensborough CC',               true,  false);
+insert into institutions values ('QNS01', 'Queens',         'Queens College',                 false, true);
+insert into institutions values ('SPH01', 'Public Health',  'CUNY School of Public Health',   false, false);
+insert into institutions values ('SPS01', 'SPS',            'School of Professional Studies', false, true);
+insert into institutions values ('YRK01', 'York',           'York College',                   false, true);
