4ec81c295ef054448e932a252d4b876e3ae3642b
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2018-04-20 00:11:03 -0400

    Working on query_check.sh
    What to do if dates don't match? Provide an override option, or insist that they
    do match.

--- query_check.sh
+++ query_check.sh
@@ -1,13 +1,24 @@
-ACAD_CAREER_TBL-15745929.csv
-Icon
-QCCV_RQMNT_DESIG_TBL.csv
-QNS_CV_ACADEMIC_ORGANIZATIONS-15685142.csv
-QNS_CV_CUNY_DISCIPLINES-15685144.csv
-QNS_CV_CUNY_SUBJECTS-15685140.csv
-QNS_CV_CUNY_SUBJECT_TABLE-15685146.csv
-QNS_CV_SR_TRNS_INTERNAL_RULES-15685158.csv
-QNS_QCCV_COURSE_ATTRIBUTES_NP-15685141.csv
-QNS_QCCV_CU_CATALOG_NP-15685155.csv
-QNS_QCCV_CU_REQUISITES_NP-15685147.csv
-SR701____INSTITUTION_TABLE-15745934.csv
-SR742A___CRSE_ATTRIBUTE_VALUE-15784962.csv
+#! /usr/local/bin/bash
+# Link the latest version of each query in queries into the latest_queries folder, dropping the
+# CUNYfirst run-id part of the file name.
+
+# Subshell to ensuring proper directories
+(
+  cd /Users/vickery/CUNY_Courses
+
+  # Clear the latest_queries folder
+  rm -f latest_queries/*
+
+  for query in  ACAD_CAREER_TBL QCCV_RQMNT_DESIG_TBL QNS_CV_ACADEMIC_ORGANIZATIONS \
+                QNS_CV_CUNY_DISCIPLINES QNS_CV_CUNY_SUBJECTS QNS_CV_CUNY_SUBJECT_TABLE \
+                QNS_CV_SR_TRNS_INTERNAL_RULES QNS_QCCV_COURSE_ATTRIBUTES_NP QNS_QCCV_CU_CATALOG_NP \
+                QNS_QCCV_CU_REQUISITES_NP SR701____INSTITUTION_TABLE SR742A___CRSE_ATTRIBUTE_VALUE
+  do
+    all=(`ls -t queries/${query}*`)
+    first=${all[0]}
+    t=${first%-*}
+    t=latest_$t
+    ln $first $t.csv
+    gstat -c "%w %n" $t.csv | cut -c 1-11,52-
+  done
+ )
