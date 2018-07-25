fe114ad822b0c5fe3c0e22f10d48fb7c5916fb44
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2018-07-25 10:22:58 -0400

    Bug fix and optimization in update_statuses.py.

--- update_statuses.py
+++ update_statuses.py
@@ -3,15 +3,16 @@
 
 import psycopg2
 from collections import namedtuple
+from psycopg2.extras import NamedTupleCursor
 
 db = psycopg2.connect('dbname=cuny_courses')
-cursor = db.cursor()
+cursor = db.cursor(cursor_factory=NamedTupleCursor)
 
 # Clear all existing status bits: only status changes from the events table
 # will be reflected in the rules table.
-cursor.execute('select count(*) from rule_groups')
-print('  Reset status for {:,} rules ...'.format(cursor.fetchone()[0]))
-cursor.execute('update rule_groups set status = 0')
+cursor.execute('select count(*) as num_rules from rule_groups where status != 0')
+print('  Reset status for {:,} rules ...'.format(cursor.fetchone().num_rules))
+cursor.execute('update rule_groups set status = 0 where status != 0')
 
 # Initialize the bitmasks dict for this script to work from
 cursor.execute('select * from review_status_bits')
@@ -29,22 +30,22 @@ events = map(Event._make, cursor.fetchall())
 for event in events:
   cursor.execute("""select status
                       from rule_groups
-                     where source_institution = '{}'
-                       and discipline = '{}'
-                       and group_number = {}
-                       and destination_institution = '{}'
-                 """.format(event.source_institution,
-                            event.discipline,
-                            event.group_number,
-                            event.destination_institution))
-  status = cursor.fetchone()[0]
+                     where source_institution = %s
+                       and source_discipline = %s
+                       and group_number = %s
+                       and destination_institution = %s
+                 """, (event.source_institution,
+                       event.discipline,
+                       event.group_number,
+                       event.destination_institution))
+  status = cursor.fetchone().status
   # print('status is {}\n  event_type is {}\n  bitmask is {}'.format(status, event.event_type, bitmasks[event.event_type]))
   status = status | bitmasks[event.event_type]
   # print('new status:', status)
   cursor.execute("""
                   update rule_groups set status = {}
                    where source_institution = '{}'
-                     and discipline = '{}'
+                     and source_discipline = '{}'
                      and group_number = {}
                      and destination_institution = '{}'
                """.format(status,
