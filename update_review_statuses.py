84c137665850359e78b247f93bfe451101906fed
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2018-08-22 10:01:48 -0400

    Decorruptificization of initialize_db.sh.
    I had overwritten the last part of it with some python code.

--- update_review_statuses.py
+++ update_review_statuses.py
@@ -11,13 +11,12 @@ cursor = db.cursor(cursor_factory=NamedTupleCursor)
 # Clear all existing status bits: only status changes from the events table
 # will be reflected in the rules table.
 cursor.execute('select count(*) as num_rules from transfer_rules where review_status != 0')
-print('  Reset status for {:,} rules ...'.format(cursor.fetchone().num_rules))
+print('\n  Reset status for {:,} rules ...'.format(cursor.fetchone().num_rules))
 cursor.execute('update transfer_rules set review_status = 0 where review_status != 0')
 
 # Initialize the bitmasks dict for this script to work from
 cursor.execute('select * from review_status_bits')
-Event_Type = namedtuple('Event_Type', [d[0] for d in cursor.description])
-event_types = map(Event_Type._make, cursor.fetchall())
+event_types = cursor.fetchall()
 bitmasks = dict()
 for event_type in event_types:
   bitmasks[event_type.abbr] = event_type.bitmask
@@ -32,7 +31,8 @@ for event in events:
                      where id = %s
                  """, (event.rule_id,))
   review_status = cursor.fetchone().review_status
-  # print('status is {}\n  event_type is {}\n  bitmask is {}'.format(status, event.event_type, bitmasks[event.event_type]))
+  # print('status is {}\n  event_type is {}\n  bitmask is {}'
+  #       .format(status, event.event_type, bitmasks[event.event_type]))
   review_status = review_status | bitmasks[event.event_type]
   # print('new status:', status)
   cursor.execute("""
