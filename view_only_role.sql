2f45e35be92cc0b9767d399edfdde87a5c19e70e
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-04-08 11:57:16 -0400

    Update view_only_role.sql
    Because it was wrong.

--- view_only_role.sql
+++ view_only_role.sql
@@ -1,5 +1,5 @@
--- Set up role query_only with select and login access
+-- Set up role view_only with select and login access
 
 drop role if exists view_only;
-create user query_only with login password 'Fernández';
+create user view_only with login password 'Fernández';
 grant select on all tables in schema public to view_only;
