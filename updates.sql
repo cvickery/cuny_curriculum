139c223aa1766e3debecc35b54b5370e73af1b4a
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-05-03 10:28:19 -0400

    Update updates.sql
    Change default file_name to 'N/A'

--- updates.sql
+++ updates.sql
@@ -1,11 +1,12 @@
--- Keep track of latest updates to the catalog and rules tables.
+-- Keep track of latest updates to the catalog and rules tables, and registered_programs table.
 drop table if exists updates;
 create table updates (
   table_name text primary key,
   update_date text default 'unknown',
-  file_name text default 'unknown');
+  file_name text default 'N/A');
 
 insert into updates values ('courses');
 insert into updates values ('transfer_rules');
 insert into updates values ('subjects');
 insert into updates values ('disciplines');
+insert into updates values ('registered_programs');
