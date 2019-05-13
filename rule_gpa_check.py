dff235c3a67ddd471947a0498329104eb76e1c9e
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-05-12 23:30:48 -0400

    Rename and Update rule_gps_check.py
    Work in progress.

--- rule_gpa_check.py
+++ rule_gpa_check.py
@@ -1,4 +1,4 @@
-""" Report rules that overlap one-another in their GPA requirements.
+""" Report rules that overlap one-another or have gaps in their GPA requirements.
     Triggered by MA 114 from QCC to QNS, which says C- or below is blanket credit, but D or above
     transfers as MATH 115.
     For every source course, look at all rule groups it belongs to, and build a list of gpa ranges;
@@ -23,32 +23,45 @@ class struct:
 
 
 def analyze(institution, course_id, ranges):
-  """ Report if a range has gaps or overlaps. In the report, tell the source course and the rule
-  sets. (think about the latter) Does not coalesce rule sets.
+  """ Given a list of gpa ranges for how a course transfers to an institution, report any cases
+      of gpa overlap and/or gaps. Accumulate sums of both types of anomaly by institution.
   """
   global problems
   if len(ranges) == 1:
-    # There can’t be an issue if there is only one rule
+    # The only possible problem would be if there is a gap
+    if ranges[0][0] > 0 or ranges[0][1] < 4.3:
+      print(f'    GAP: {institution} {course_id:06} {ranges[0][0]} - {ranges[0][1]}')
+      gaps[(institution, frozenset(ranges))] += 1
     return
-    # Sort the ranges and be sure, for each pair, that there is no gap and no overlap
-  ranges_ok = True
+
+  # Sort the ranges, which assures only that their minima are in sequence. If the maxima are out of
+  # sequence, that will show up as overlaps. Assumes max_gpa is always >= min_gpa. (No exception at
+  # this time (May, 2019)).
   ranges = sorted(ranges)
-  prev = ranges[0]
+
+  range_min, range_max = ranges[0]
+  gaps_ok = range_min == 0
+  laps_ok = True  # ’laps, as in overlaps
   for range in ranges[1:]:
-    if (prev[1] > range[0]) or (abs(range[0] - prev[1]) > 0.3):
-      ranges_ok = False
-      break
-    prev = range
-  if ranges_ok:
-    return
-  # Add to the set of problems
-  problems[(institution, frozenset(ranges))] += 1
+    if range[0] - range_max > 0.3:
+      gaps_ok = False
+    if range_max - range[1] > 0.3:  # TODO
+      laps_ok = False
+    range_max = range[1]
+  if not gaps_ok:
+    print(f'    GAP: {institution} {course_id:06} {ranges[0][0]} - {ranges[0][1]}')
+    gaps[(institution, frozenset(ranges))] += 1
+  if not laps_ok:
+    print(f'    LAP: {institution} {course_id:06} {ranges[0][0]} - {ranges[0][1]}')
+    laps[(institution, frozenset(ranges))] += 1
 
 
 conn = psycopg2.connect('dbname=cuny_courses')
 cursor = conn.cursor(cursor_factory=NamedTupleCursor)
 
-problems = defaultdict(int)
+laps = defaultdict(int)
+gaps = defaultdict(int)
+
 previous_row = struct(course_id=-1, institution='')
 cursor.execute("""
                 select r.destination_institution as institution, r.id, s.course_id, min_gpa, max_gpa
@@ -62,10 +75,17 @@ for row in cursor.fetchall():
     if len(ranges) > 1:
       analyze(previous_row.institution, previous_row.course_id, ranges)
     ranges = set()
-  ranges.add((row.min_gpa, min(4.5, row.max_gpa)))
+  ranges.add((row.min_gpa, min(4.3, row.max_gpa)))
   previous_row = row
 
-for key, count in problems.items():
+print('Gap counts')
+for key, count in gaps.items():
+  institution = key[0]
+  tuples = str(sorted(key[1]))
+  print(f'{institution} {tuples:<54} {count:4}')
+
+print('Overlap counts')
+for key, count in laps.items():
   institution = key[0]
   tuples = str(sorted(key[1]))
   print(f'{institution} {tuples:<54} {count:4}')
