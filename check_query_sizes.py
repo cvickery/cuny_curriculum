2fd5f0ac0386f051d71149a601470d5191b91976
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-06-19 23:01:32 -0400

    Update check_query_sizes
    Eliminate need for db table. Just check files in queries against corresponding
    files in latest queries.

--- check_query_sizes.py
+++ check_query_sizes.py
@@ -1,55 +1,33 @@
 #! /usr/local/bin/python3
-""" Some queries have been coming in truncated. This utility manages a database of query sizes
-    and notes when an anomaly is found.
+""" Some queries have been coming in truncated. This utility checks the files in queries for
+    emptyness and compares sizes with the corresponding files in latest_queries for size differences
+    of 10% or more.
 """
-import os
-import psycopg2
-from psycopg2.extras import NamedTupleCursor
+from pathlib import Path
+import argparse
 
-db = psycopg2.connect('dbname=cuny_courses')
-cursor = db.cursor(cursor_factory=NamedTupleCursor)
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true')
+args = parser.parse_args()
 
-cursor.execute("""
-  create table if not exists query_sizes (
-  id serial primary key,
-  query_date timestamp with time zone default current_timestamp,
-  query_name text not null,
-  query_size integer
-  );
-  """)
+new_queries = Path('/Users/vickery/CUNY_Courses/queries')
+previous_queries = Path('/Users/vickery/CUNY_Courses/latest_queries/')
 
-latest_queries = '/Users/vickery/CUNY_Courses/latest_queries/'
-fail = False
-for file in os.listdir(latest_queries):
-  if file.endswith('.csv'):
-    file_size = os.stat(f'{latest_queries}/{file}').st_size
-    file_name = file.lower()
-    cursor.execute(""" select query_date, query_size
-                       from query_sizes
-                       where query_name = %s
-                    """, (file_name, ))
-    if cursor.rowcount == 0:
-      print(f'Init {file_name}: {file_size:,} bytes')
-      cursor.execute('insert into query_sizes values (default, default, %s, %s)',
-                     (file_name, file_size))
-      if cursor.rowcount != 1:
-        exit('Insert failed!')
-    else:
-      previous_date, previous_size = cursor.fetchone()
-      if abs(previous_size - file_size) > (0.1 * previous_size):
-        print(f'FAIL: {file_name} ({file_size:,} bytes) is more than 10% different from '
-              f'{previous_size:,} bytes'
-              f' on {previous_date.strftime("%Y-%m-%d %I:%M %p")}')
-        fail = True
-      else:
-        print(f'Update {file_name}: {file_size:,} bytes')
-        cursor.execute('update query_sizes set query_size = %s where query_name = %s',
-                       (file_size, file_name))
-        if cursor.rowcount != 1:
-          exit('Update failed!')
-db.commit()
-db.close()
 
-if fail:
-  exit('Query size error.')
+for previous_query in previous_queries.glob('*.csv'):
+  new_query = [q for q in new_queries.glob(f'{previous_query.stem}*.csv')]
+  if len(new_query) == 0:
+    exit(f'No new query for {previous_query.name}')
+  assert len(new_query) == 1, f'{len(new_query)} matches for {previous_query.name}'
+  new_query = new_query[0]
+  previous_size = previous_query.stat().st_size
+  new_size = new_query.stat().st_size
+  if new_size == 0:
+    exit(f'{new_query.name} has zero bytes')
+  if abs(previous_size - new_size) > 0.1 * previous_size:
+    exit('{} ({}) differs from {} ({}) by more than 10%'
+         .format(new_query.name, new_size, previous_query.name, previous_size))
+  if args.verbose:
+    print(f'{new_query.name} compares favorably to {previous_query.name}')
+# No problems detected
 exit(0)
