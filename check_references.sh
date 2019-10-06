4f7dc3348c564f71ceb5ee5ba88ba4426cd48248
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-10-05 22:43:43 -0400

    Add check_references.sh; update run_control_ids
    The shell script is a utility for verifying that the query files checked by
    check_queries are actually referenced by .py files.
    
    This does _not_ check if all queries referenced by python scripts are checked by
    check_queries.py

---
+++ check_references.sh
@@ -0,0 +1,11 @@
+#! /usr/local/bin/bash
+
+# Check that all queries that check_queries checks are actually referenced by a python script
+# somewhere in this directory.
+
+ok='ok'
+for query in `./check_queries.py --list`
+do ack $query *.py > /dev/null
+  [[ $? != 0 ]] &&  ok='not ok' echo $query 'not referenced in *.py'
+done
+echo $ok
