c17d5e8a4ec1d747777a3d3937cfbaf97fd37230
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-11-06 09:41:44 -0500

    Add cuny_subjects_by_college.sql
    This is a utility query, not part of the db initialization.

---
+++ cuny_subjects_by_college.sql
@@ -0,0 +1,12 @@
+-- what colleges offer courses in what cuny subjects?
+copy (
+select cuny_subject,
+       subject_name,
+       string_agg(distinct institution, ', ') as colleges
+from courses, cuny_subjects
+where subject = cuny_subject
+  and course_status = 'A'
+  and can_schedule = 'Y'
+  and attributes !~ 'BKCR'
+group by cuny_subject, subject_name
+order by cuny_subject, colleges) to '/Users/vickery/Desktop/cuny_subjects_by_college.csv' header csv
