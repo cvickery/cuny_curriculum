773b04184b9bf0da214734c1c782b5d6ca9c916c
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-12-27 23:06:11 -0500

    Update updates.sql
    Migrating tables from cuny_programs to cuny_courses.

--- updates.sql
+++ updates.sql
@@ -1,12 +1,14 @@
--- Keep track of latest updates to the catalog and rules tables, and registered_programs table.
+-- Keep track of latest updates to various tables.
 drop table if exists updates;
 create table updates (
   table_name text primary key,
-  update_date text default 'unknown',
+  update_date text,
   file_name text default 'N/A');
 
 insert into updates values ('courses');
-insert into updates values ('transfer_rules');
-insert into updates values ('subjects');
 insert into updates values ('disciplines');
+insert into updates values ('hegis_codes');
 insert into updates values ('registered_programs');
+insert into updates values ('requirement_blocks');
+insert into updates values ('subjects');
+insert into updates values ('transfer_rules');
