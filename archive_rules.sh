312f4b92d7fc91f06fca05da8634bd30ca9fa4d4
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-06-13 09:21:19 -0400

    Update archive_rules.sh
    Fix datestamp.

--- archive_rules.sh
+++ archive_rules.sh
@@ -12,7 +12,7 @@ psql -Xqd cuny_curriculum -c "copy (select  rule_key(rule_id) as rule_key, \
                               min_gpa, \
                               max_gpa \
                       from source_courses) to \
-                      '`pwd`/rules_archive/``date +"%Y-%m-%d"``_source_courses.csv' csv"
+                      '`pwd`/rules_archive/`date +"%Y-%m-%d"`_source_courses.csv' csv"
 echo done
 
 echo -n "destination_courses ... "
@@ -21,7 +21,7 @@ psql -Xqd cuny_curriculum -c "copy (select  rule_key(rule_id) as rule_key, \
                               offer_nbr, \
                               transfer_credits \
                       from destination_courses) to \
-                      '`pwd`/rules_archive/``date +"%Y-%m-%d"``_destination_courses.csv' csv"
+                      '`pwd`/rules_archive/`date +"%Y-%m-%d"`_destination_courses.csv' csv"
 echo done
 
 echo $SECONDS seconds
