bc4074c1818014caa4ca486e09d631c09e368ea7
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-06-13 07:59:22 -0400

    Update archive_rules.sh
    Use system date command instead of GNU date command.

--- archive_rules.sh
+++ archive_rules.sh
@@ -2,15 +2,6 @@
 
 SECONDS=0
 
-# echo -n "transfer_rules ... "
-# psql -Xqd cuny_curriculum -c "copy (select  id, \
-#                               source_institution, \
-#                               destination_institution, \
-#                               subject_area, group_number \
-#                       from transfer_rules) to \
-#                       '`pwd`/rules_archive/`date -I`_transfer_rules.csv' csv"
-# echo done
-
 echo -n "source_courses ... "
 psql -Xqd cuny_curriculum -c "copy (select  rule_key(rule_id) as rule_key, \
                               course_id, \
@@ -21,7 +12,7 @@ psql -Xqd cuny_curriculum -c "copy (select  rule_key(rule_id) as rule_key, \
                               min_gpa, \
                               max_gpa \
                       from source_courses) to \
-                      '`pwd`/rules_archive/`date -I`_source_courses.csv' csv"
+                      '`pwd`/rules_archive/``date +"%Y-%m-%d"``_source_courses.csv' csv"
 echo done
 
 echo -n "destination_courses ... "
@@ -30,7 +21,7 @@ psql -Xqd cuny_curriculum -c "copy (select  rule_key(rule_id) as rule_key, \
                               offer_nbr, \
                               transfer_credits \
                       from destination_courses) to \
-                      '`pwd`/rules_archive/`date -I`_destination_courses.csv' csv"
+                      '`pwd`/rules_archive/``date +"%Y-%m-%d"``_destination_courses.csv' csv"
 echo done
 
 echo $SECONDS seconds
