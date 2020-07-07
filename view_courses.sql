a1b69061e17336410f358daec6e9c33b5ad9ea22
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-07-06 22:57:12 -0400

    Update view_courses.sql
    Make it less cryptic.

--- view_courses.sql
+++ view_courses.sql
@@ -2,18 +2,16 @@
 --    Truncate title; omit description and attributes.
 --    Used during development to look up courses quickly.
 drop view if exists view_courses cascade;
-create view view_courses as
-(
-  select lpad(course_id::text, 6, '0') as id,
-         offer_nbr as "#",
-         institution as coll,
-         discipline as discp,
-         numeric_part(catalog_number) as cat_num,
-         substr(title, 1, 40) as title,
-         contact_hours as hr,
-         min_credits||'-'||max_credits as cr,
-         course_status as status,
-         designation,
-         attributes
+create view view_courses as (
+    select institution,
+           lpad(course_id::text, 6, '0') as course_id,
+           discipline,
+           catalog_number,
+           contact_hours,
+           min_credits,
+           max_credits,
+           designation,
+           attributes,
+           course_status
     from cuny_courses
-order by institution, discipline, cat_num);
+order by institution, discipline, catalog_number);
