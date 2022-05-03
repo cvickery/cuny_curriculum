4facd771c983e3f1a1abe1be4731ceec3df4babb
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-05-03 11:02:20 -0400

    Clean up view_course

--- view_courses.sql
+++ view_courses.sql
@@ -1,21 +1,24 @@
---  View fields of courses that will fit on one line.
---    Truncate title and attributes; omit description.
---    Used during development to look up courses quickly.
-drop view if exists view_courses cascade;
+drop view if exists view_courses;
 create view view_courses as (
-    select institution,
-           lpad(course_id::text, 6, '0') as course_id,
-           offer_nbr,
-           discipline,
-           catalog_number,
-           substr(title, 0, 32)||
-           case when (length(title) > 32) then '...' else '' end as title,
-           contact_hours,
-           min_credits,
-           max_credits,
-           designation,
-           substr(attributes, 0, 16)||
-           case when (length(attributes) > 16) then '...' else '' end as attributes,
-           course_status
-    from cuny_courses
-order by institution, discipline, catalog_number);
+SELECT cuny_courses.institution,
+    cuny_courses.course_id as id,
+    cuny_courses.offer_nbr as nr,
+    cuny_courses.discipline||' '||cuny_courses.catalog_number as course,
+    substr(cuny_courses.title, 0, 32) ||
+        CASE
+            WHEN length(cuny_courses.title) > 32 THEN '...'::text
+            ELSE ''::text
+        END AS title,
+    cuny_courses.contact_hours as hr,
+    cuny_courses.min_credits as min,
+    cuny_courses.max_credits as max,
+    cuny_courses.designation as rd,
+    cuny_courses.course_status as stat,
+    substr(cuny_courses.attributes, 0, 16) ||
+        CASE
+            WHEN length(cuny_courses.attributes) > 16 THEN '...'::text
+            ELSE ''::text
+        END AS attr
+   FROM cuny_courses
+  ORDER BY cuny_courses.institution, cuny_courses.discipline, cuny_courses.catalog_number
+);
