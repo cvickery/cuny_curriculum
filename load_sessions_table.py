c7c73a7312d89b90b2441bd85dca231947232fb8
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-10-18 23:54:25 -0400

    Use corrected sessions table query

--- load_sessions_table.py
+++ load_sessions_table.py
@@ -6,16 +6,32 @@ import csv
 import psycopg
 import sys
 
+from collections import namedtuple
 from psycopg.rows import namedtuple_row
 
+missing_date = '1901-01-01'
 with psycopg.connect('dbname=cuny_curriculum') as conn:
   with conn.cursor(row_factory=namedtuple_row) as cursor:
-    with open('./latest_queries/SESSION_TABLE.csv') as csv_file:
+    with open('./latest_queries/QNS_CV_SESSION_TABLE.csv') as csv_file:
       reader = csv.reader(csv_file)
       for line in reader:
         if reader.line_num == 1:
           col_names = [col_name.lower().replace(' ', '_') for col_name in line]
-          schema_rows = [f'{col_name} TEXT' for col_name in col_names]
+          Row = namedtuple('Row', col_names)
+          schema_rows = []
+          for col_name in col_names:
+            if col_name == 'term':
+              schema_rows.append('term integer')
+            elif col_name in ['session_beginning_date',
+                              'session_end_date',
+                              'open_enrollment_date',
+                              'first_date_to_enroll',
+                              'last_date_to_enroll',
+                              'census_date',
+                              'sysdate']:
+              schema_rows.append(f'{col_name} DATE')
+            else:
+              schema_rows.append(f'{col_name} TEXT')
           placeholders = ('%s, ' * len(schema_rows)).strip(' ,')
           schema = ','.join(schema_rows)
           cursor.execute(f"""
@@ -23,6 +39,8 @@ with psycopg.connect('dbname=cuny_curriculum') as conn:
           create table cuny_sessions ({schema}, PRIMARY KEY (institution, career, term, session))
           """)
         else:
+          row = Row._make(line)
+          values = [missing_date if value == '' else value for value in row]
           cursor.execute(f"""
           insert into cuny_sessions values ({placeholders})
-          """, line)
+          """, values)
