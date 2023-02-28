28197d070f9ccbe4811e4347f2bca98232b52c54
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2023-02-28 17:41:09 -0500

    Handle copy to when server is not local

--- archive_rules.sh
+++ archive_rules.sh
@@ -7,35 +7,38 @@ update_date=${update_date// /}
 echo Archiving $update_date
 
 echo -n "source_courses ... "
-psql -Xqd cuny_curriculum -c "copy (select  rule_key(rule_id) as rule_key, \
-                              course_id, \
-                              offer_nbr, \
-                              min_credits, \
-                              max_credits, \
-                              credit_source, \
-                              min_gpa, \
-                              max_gpa \
-                      from source_courses) to \
-                      '`pwd`/rules_archive/${update_date}_source_courses.csv' csv"
+psql -Xqd cuny_curriculum <<EOD >rules_archive/${update_date}_source_courses.csv
+copy (select rule_key(rule_id) as rule_key,
+             course_id,
+             offer_nbr,
+             min_credits,
+             max_credits,
+             credit_source,
+             min_gpa,
+             max_gpa
+        from source_courses) to stdout csv
+EOD
 echo done
 
 echo -n "destination_courses ... "
-psql -Xqd cuny_curriculum -c "copy (select  rule_key(rule_id) as rule_key, \
-                              course_id, \
-                              offer_nbr, \
-                              transfer_credits \
-                      from destination_courses) to \
-                      '`pwd`/rules_archive/${update_date}_destination_courses.csv' csv"
+psql -Xqd cuny_curriculum <<EOD >rules_archive/${update_date}_destination_courses.csv
+copy (select rule_key(rule_id) as rule_key,
+             course_id,
+             offer_nbr,
+             transfer_credits
+        from destination_courses) to stdout csv
+EOD
 echo done
 
 echo -n "effective_dates ... "
-psql -Xqd cuny_curriculum -c "copy (select  rule_key(id) as rule_key, \
-                              effective_date \
-                      from transfer_rules) to \
-                      '`pwd`/rules_archive/${update_date}_effective_dates.csv' csv"
+psql -Xqd cuny_curriculum <<EOD >rules_archive/${update_date}_effective_dates.csv
+copy (select rule_key(id) as rule_key,
+             effective_date
+        from transfer_rules) to stdout csv
+EOD
 echo done
 
 echo Compressing
-bzip2 -f `pwd`/rules_archive/*.csv
+bzip2 -f rules_archive/*.csv
 
 echo $SECONDS seconds
