dddb26450c1074f10df18b7d33264a7ea3ed192a
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2023-03-19 14:16:29 -0400

    Clarify cuny_sessions table.

--- load_sessions_table.py
+++ load_sessions_table.py
@@ -1,46 +1,70 @@
 #! /usr/local/bin/python3
-""" (Re-)create table of undergraduate sessions.
-"""
+"""Create sessions table for 2010 to now.
 
+Renaming CUNYfirst fields:
+  Institution             institution
+  Term                    term
+  Session                 session
+  Session Beginning Date  start_classes
+  Session End Date        end_classes
+  First Date to Enroll    early_enrollment
+  Open Enrollment Date    open_enrollment
+  Last Date to Enroll     last_enrollment
+  Census Date             census_date
+
+"""
 import csv
 import psycopg
-import sys
 
 from collections import namedtuple
 from psycopg.rows import namedtuple_row
 
-missing_date = '1901-01-01'
 with psycopg.connect('dbname=cuny_curriculum') as conn:
-  with conn.cursor(row_factory=namedtuple_row) as cursor:
-    with open('./latest_queries/QNS_CV_SESSION_TABLE.csv') as csv_file:
-      reader = csv.reader(csv_file)
+  with conn.cursor() as cursor:
+
+    cursor.execute("""
+    drop table if exists cuny_sessions;
+    create table cuny_sessions (
+    institution           text,
+    term                  integer,
+    session               text,
+    early_enrollment      date default null,
+    open_enrollment       date default null,
+    last_enrollment       date default null,
+    classes_start         date default null,
+    census_date           date default null,
+    classes_end           date default null,
+    primary key (institution, term, session)
+    )
+    """)
+    csv_to_db = {'first_date_to_enroll': 'early_enrollment',
+                 'open_enrollment_date': 'open_enrollment',
+                 'last_date_to_enroll': 'last_enrollment',
+                 'session_beginning_date': 'classes_start',
+                 'census_date': 'census_date',
+                 'session_end_date': 'classes_end'
+                 }
+    with open('./latest_queries/QNS_CV_SESSION_TABLE.csv') as sess:
+      reader = csv.reader(sess)
       for line in reader:
         if reader.line_num == 1:
-          col_names = [col_name.lower().replace(' ', '_') for col_name in line]
-          Row = namedtuple('Row', col_names)
-          schema_rows = []
-          for col_name in col_names:
-            if col_name == 'term':
-              schema_rows.append('term integer')
-            elif col_name in ['session_beginning_date',
-                              'session_end_date',
-                              'open_enrollment_date',
-                              'first_date_to_enroll',
-                              'last_date_to_enroll',
-                              'census_date',
-                              'sysdate']:
-              schema_rows.append(f'{col_name} DATE')
-            else:
-              schema_rows.append(f'{col_name} TEXT')
-          placeholders = ('%s, ' * len(schema_rows)).strip(' ,')
-          schema = ','.join(schema_rows)
-          cursor.execute(f"""
-          drop table if exists cuny_sessions;
-          create table cuny_sessions ({schema}, PRIMARY KEY (institution, career, term, session))
-          """)
+          cols = [c.lower().replace(' ', '_') for c in line]
         else:
-          row = Row._make(line)
-          values = [missing_date if value == '' else value for value in row]
-          cursor.execute(f"""
-          insert into cuny_sessions values ({placeholders})
-          """, values)
+          row = dict()
+          for index, value in enumerate(line):
+            row[cols[index]] = value
+          if row['career'].startswith('U'):
+            column_names = ['institution', 'term', 'session']
+            placeholders = '%s, %s, %s'
+            values = [row['institution'], row['term'], row['session']]
+            # Handle missing dates
+            for field in ['first_date_to_enroll', 'open_enrollment_date', 'last_date_to_enroll',
+                          'session_beginning_date', 'census_date', 'session_end_date']:
+                if row[field]:
+                  column_names.append(csv_to_db[field])
+                  placeholders += ', %s'
+                  values.append(row[field])
+            column_names = ', '.join(column_names)
+            cursor.execute(f"""
+            insert into cuny_sessions ({column_names}) values ({placeholders})
+            """, values)
